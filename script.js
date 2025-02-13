c01f39815866b408be467d1899c2995dac209590
PMI EDUVERSE <pmieduverse@gmail.com>
2025-02-13 11:48:13 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add active class to the current navigation link
-document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('.nav-link');
-    links.forEach(link => {
-        if (link.href === window.location.href) {
-            link.style.color = '#32FF7E'; // Highlight the current page
-        }
-    });
-});
